daed8d2a515fff0310ade182c8d2f2dd2586cfff
Michal Malý <madcatxster@gmail.com>
2019-01-20 14:00:33 +0100

    Avoid ambiguous response of the return key by accepting a widget from ReturnAcceptableTableView by reacting to Ctrl+Return

--- src/ui/returnacceptabletableview.cpp
+++ src/ui/returnacceptabletableview.cpp
@@ -11,7 +11,7 @@ void ReturnAcceptableTableView::keyPressEvent(QKeyEvent *evt)
 {
   CopiableItemsTableView::keyPressEvent(evt);
 
-  if (evt->key() == Qt::Key_Return) {
+  if (evt->key() == Qt::Key_Return && evt->modifiers() == Qt::CTRL) {
     /* This is necessary to make sure that
      * any edited value gets commited */
     if (!hasFocus())
